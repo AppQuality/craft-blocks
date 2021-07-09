034235b5ab0d90ce26998bfea92650dc9bf468cf
Davide Bizzi <davide.bizzi@app-quality.com>
2021-07-09 11:53:14 +0200

    fix: Copy modules and css

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export * from './Wysiwyg/index';
-export * from './Button';
-export * from './ButtonContainer';
-export * from './Container';
-export * from './Text';
-export { Editor, Frame, Element } from '@craftjs/core';
+export * from "./Wysiwyg/index";
+export * from "./Button";
+export * from "./ButtonContainer";
+export * from "./Container";
+export * from "./Text";
+export { Editor, Frame, Element, useEditor } from "@craftjs/core";
