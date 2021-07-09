c17cfe20b669fdc1dd8aa348b148792983cfe51e
Davide Bizzi <davide.bizzi@app-quality.com>
2021-07-09 14:14:11 +0200

    wip: Remove craftjs from export

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export * from "./Button";
 export * from "./ButtonContainer";
 export * from "./Container";
 export * from "./Text";
-export { Editor, Frame, Element, useEditor } from "@craftjs/core";
